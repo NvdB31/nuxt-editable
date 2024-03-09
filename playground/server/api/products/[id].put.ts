d8ae94f3143a134b993930a87f7d8d57201f09d9
Nick van den berg <nickvandenberg31@gmail.com>
2024-03-09 22:43:15 +0100

    chore: update deps

--- playground/server/api/products/[id].put.ts
+++ playground/server/api/products/[id].put.ts
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
-import models from '../../models'
-
 export default defineEventHandler( async (event) => {
     const id = getRouterParam(event, 'id');
     const body = await readBody(event)
     
-    const model = models.posts
-    return model.updateOne({ _id: new mongoose.Types.ObjectId(id) }, body);
+    return $fetch(`https://dummyjson.com/products/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    })
 })
